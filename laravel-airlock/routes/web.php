46705092b115ad7937259786f73c5e08ac0c61c9
大西雄貴 <yuki.onishi1129@gmail.com>
2020-06-17 23:24:41 +0900

    /create_token: user_id=1のデータに対し、tokenを作成しテーブルに保存するためのメソッド(token作成の練習のため、作成)

--- laravel-airlock/routes/web.php
+++ laravel-airlock/routes/web.php
@@ -22,8 +22,12 @@ Route::get('/create_token', function () {
     // usersテーブルのid:1のデータを取得(userインスタンスを生成)
     $user = User::find(1);
     // Laravel\Sanctum\HasApiTokensのメソッド
+    // personal_access_tokensテーブルのtokenにusersテーブルのレコードに紐づくデータを生成
+    // 紐づくtokenを生成し、テーブルに保存する
     $token = $user->createToken('my-api-token');
     echo $token->plainTextToken; //トークンを表示する
+    // $token->plainTextTokenで表示されるトークンは
+    // personal_access_tokensテーブルのtokenに表示されている値とは異なるが、これはよりセキュアにするために暗号化されているため同じ
 });
 
 Auth::routes();
